f40224bfb69799b4e85faf28f2d8a59ce37ae488
Mikael Nordberg <nordbergmikael@hotmail.com>
2010-12-09 11:48:32 +0100

    Added sqlite sessionsource provider...

---
+++ SqLiteInMemorySessionSourceProvider.cs
@@ -0,0 +1,33 @@
+﻿using FluentNHibernate;
+using FluentNHibernate.Cfg.Db;
+using NullReference.Data.Events;
+
+namespace NullReference.Data
+{
+    public class SqLiteInMemorySessionSourceProvider : ISessionSourceProvider
+    {
+        readonly IPersistenceMappings _mappings;
+
+        public SqLiteInMemorySessionSourceProvider(IPersistenceMappings mappings)
+        {
+            _mappings = mappings;
+        }
+
+        public ISessionSource GetSessionSource()
+        {
+            var config = _mappings.Generate()
+                .Database(SQLiteConfiguration.Standard.InMemory()
+                              .ProxyFactoryFactory(typeof(NHibernate.ByteCode.LinFu.ProxyFactoryFactory)))
+                .ExposeConfiguration(EventListenerRegistrations.Apply);
+
+            var sessionSource = new SessionSource(config);
+
+            if (ShouldGenerateDb)
+                sessionSource.BuildSchema();
+
+            return sessionSource;
+        }
+
+        public bool ShouldGenerateDb { get; set; }
+    }
+}
